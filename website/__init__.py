88b4e29aadd744b9b3fe9066254a3e36a0bb3c4f
siberkolosis <siberkolosis@gmail.com>
2021-10-10 20:40:30 +0700

    ok

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,31 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'kunci rahasia'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mssql+pymssql://sa:123456@10.89.1.50:1433/VMS'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+    from .models import User, Note
+    db.create_all(app=app)
     
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views,url_prefix='/')
     app.register_blueprint(auth,url_prefix='/')
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
