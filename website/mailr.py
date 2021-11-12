7eae8aa6e6905c712ff186986cd7399cccf12db1
aFarisk <blgr182@gmail.com>
2021-11-13 02:55:28 +0700

    button decline permit3 finish

--- website/mailr.py
+++ website/mailr.py
@@ -1,4 +1,5 @@
 import smtplib
+from email.message import EmailMessage
 
 def kirimEmail(to,subject,body):
     msg = EmailMessage()
@@ -9,38 +10,10 @@ def kirimEmail(to,subject,body):
     msg['To'] = to
 
     # Send the message via our own SMTP server.
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.login("me@gmail.com", "password")
+    server = smtplib.SMTP_SSL('mail.privateemail.com', 465)
+    server.login("info@siberkolosis.com", "grunge75")
     server.send_message(msg)
     server.quit()
 
+# kirimEmail('info@siberkolosis.com','ditolak','selamat anda di tolak')
 
-'''
-def kirimEmail(to,subject,body):
-    gmail_user = 'info@siberkolosis.com'
-    gmail_password = 'grunge75'
-
-    sent_from = gmail_user
-    to = [to]
-    subject = subject
-    body = body
-
-    email_text = """\
-    From: %s
-    To: %s
-    Subject: %s
-
-    %s
-    """ % (sent_from, ", ".join(to), subject, body)
-
-    try:
-        smtp_server = smtplib.SMTP_SSL('mail.privateemail.com', 465)
-        smtp_server.ehlo()
-        # smtp_server.starttls()
-        smtp_server.login(gmail_user, gmail_password)
-        smtp_server.sendmail(sent_from, to, email_text)
-        smtp_server.close()
-        print ("Email sent successfully!")
-    except Exception as ex:
-        print ("Something went wrong….",ex)
-'''
