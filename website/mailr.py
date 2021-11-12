aefab8f33df182e97c5c1c386d4bf9feaf2e34f1
aFarisk <blgr182@gmail.com>
2021-11-13 02:46:20 +0700

    button decline permit2

--- website/mailr.py
+++ website/mailr.py
@@ -1,6 +1,22 @@
 import smtplib
 
 def kirimEmail(to,subject,body):
+    msg = EmailMessage()
+    msg.set_content(body)
+
+    msg['Subject'] = subject
+    msg['From'] = 'info@siberkolosis.com'
+    msg['To'] = to
+
+    # Send the message via our own SMTP server.
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.login("me@gmail.com", "password")
+    server.send_message(msg)
+    server.quit()
+
+
+'''
+def kirimEmail(to,subject,body):
     gmail_user = 'info@siberkolosis.com'
     gmail_password = 'grunge75'
 
@@ -27,3 +43,4 @@ def kirimEmail(to,subject,body):
         print ("Email sent successfully!")
     except Exception as ex:
         print ("Something went wrong….",ex)
+'''
